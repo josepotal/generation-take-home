10e7e328f1f86bc4b65a33790eb681b03fc34bf0
josepotal <j.otal.pares@gmail.com>
2017-11-07 18:02:06 -0600

    Add conversion address coords

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
 
-
-// we import the json data locally
-import hola from 'json!../../mini-store_directory.json'
-console.log(hola.items)
-
-const INITIAL_LOCATION = {
-  position: {
-    latitude: 19.43236,
-    longitude: -99.1332
-  }
-};
-
-const stores = [
-  ['Loc1', 19.389882, -99.179829],
-  ['Loc2', 19.401774, -99.176698],
-  ['Loc3', 19.426993, -99.116713],
-  ['Loc4', 19.459940, -99.139295]
-];
+import stores from 'json!../../mini-store_directory.json'
+console.log(stores)
+//import data from '../utils/address-converter';
 
 export default class Map extends Component {
   constructor(){
     super()
     this.setMarkers = this.setMarkers.bind(this)
+    //this.geocodeAddress = this.geocodeAddress.bind(this)
   }
 
   componentDidMount() {
@@ -32,26 +18,55 @@ export default class Map extends Component {
       zoom: 12
     });
 
-    this.marker = new google.maps.Marker({
-      map: this.map,
-      position: {
-        lat: INITIAL_LOCATION.position.latitude,
-        lng: INITIAL_LOCATION.position.longitude
-      }
-    });
-
     this.setMarkers(this.map)
+
+    //this.geocoder = new google.maps.Geocoder();
+
+
+    //json.items.map((item)=>this.geocodeAddress(item, this.geocoder))
   }
 
   setMarkers = map => {
-    stores.map(stores => {
-      const marker = new google.maps.Marker({
-        position: {lat: stores[1], lng: stores[2]},
-        map: map,
-        title: stores[0]
-      })
+    stores.items.map(store => {
+      if( store.loc.length > 0){
+        const marker = new google.maps.Marker({
+          position: {lat: store.loc[0], lng: store.loc[1]},
+          map: map,
+          title: store.Name
+        })
+      }
+
     })
   }
+
+
+//   geocodeAddress = (item, geocoder) => {
+//     var loc=[];
+//     geocoder.geocode({ 'address': item.Address }, (results, status) => {
+//     console.log(status, results)
+//
+//     if (status === google.maps.GeocoderStatus.OK) {
+//
+//       //this.map.setCenter(results[0].geometry.location);
+//       loc[0]=results[0].geometry.location.lat();
+//       loc[1]=results[0].geometry.location.lng();
+//       console.log(loc)
+//       //this.marker.setPosition(loc);
+//       const marker = new google.maps.Marker({
+//         position: {lat: loc[0], lng: loc[1]},
+//         map: this.map,
+//         title: item.Name
+//       })
+//       return;
+//     } else {
+//       if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+//         console.log('exceeded')
+//       }
+//     }
+//
+//   })
+// }
+
 
   render() {
     return (
---
+++ src/utils/address-converter.js
@@ -0,0 +1,80 @@
+// we import the json data locally
+import data from 'json!../../store_directory.json'
+
+console.log(data.items.length)
+let sliced1Data = data.items.slice(42, 50);
+console.log(sliced1Data)
+let newArray = []
+
+sliced1Data.map((item, index) => {
+  //console.log(geocodeAddress(item))
+  const loc = geocodeAddress(item)
+  console.log(loc)
+  item = {...item, loc}
+  newArray.push(item)
+})
+console.log(newArray)
+
+export default data;
+
+
+function geocodeAddress(item){
+  var geocoder = new google.maps.Geocoder();
+  var loc=[];
+    geocoder.geocode({ 'address': item.Address }, (results, status) => {
+
+      if(status === google.maps.GeocoderStatus.OK) {
+        let lat=results[0].geometry.location.lat();
+        let lng=results[0].geometry.location.lng();
+        loc.push(lat)
+        loc.push(lng)
+        return;
+      }
+      // } else {
+      //   if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+      //     console.log('exceeded')
+      //   }
+      // }
+  })
+  return loc
+}
+
+// var id = setInterval(function(){
+//   console.log('time')
+// }, 1000)
+
+
+//========================================
+// we import the json data locally
+// import data from 'json!../../mini-store_directory.json'
+// console.log(data.items[0].Address)
+//
+// const item = data.items[0]
+// console.log(item)
+// var loc = codeAddress(data.items[0].Address.toLowerCase())
+//
+// console.log(loc)
+//
+// export default data;
+
+// // helper function to convert address to latitude and longitude
+// function codeAddress(address) {
+//     var geocoder = new google.maps.Geocoder();
+//     var loc=[];
+//
+//     // next line creates asynchronous request
+//     geocoder.geocode( { 'address': address}, function(results, status) {
+//       // and this is function which processes response
+//       if (status == google.maps.GeocoderStatus.OK) {
+//         loc[0]=results[0].geometry.location.lat();
+//         loc[1]=results[0].geometry.location.lng();
+//
+//         alert( loc ); // the place where loc contains geocoded coordinates
+//
+//       } else {
+//         alert('Geocode was not successful for the following reason: ' + status);
+//       }
+//     });
+//
+//     return loc;
+// }
